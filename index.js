832f15fcdd4a26dcce579f2c37d5f55e84de31ac
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-19 13:44:36 +0300

    done

--- index.js
+++ index.js
@@ -1,34 +1,104 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    // Write your algorithm here
+    for (let i = 0; i < array.length; i++) {
+        const complement = target - array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] === complement) return true;
+        }
+    }
+    return false;
 }
+
+function hasTargetSum(array, target) {
+    const seenNumbers = new Set(); // initialize an empty Set
+    for (const number of array) {
+        const complement = target - number;
+
+        // .has returns true if the Set includes the complement
+        if (seenNumbers.has(complement)) return true;
+
+        // .add adds the number to the Set
+        seenNumbers.add(number);
+    }
+    return false;
+}
+
 
 /* 
   Write the Big O time complexity of your function here
+  time complexity = O(n²)
+  space complexity = O(1) - constant
 */
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
+complementary = difference
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution herecreate an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+    console.log("");
+
+    console.log("");
+    // Negative numbers?
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+    console.log("");
+    // Multiple pairs?
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+    console.log("");
+    // Single numbers?
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([4], 4));
 }
+
+module.exports = hasTargetSum;
+console.log("");
+// Single numbers?
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([4], 4));
 
 module.exports = hasTargetSum;
